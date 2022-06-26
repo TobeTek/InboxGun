939b1610d451d6627520e19042fb2d651dfc4bd0
TobeTek <katchyemma@gmail.com>
2022-06-26 16:45:46 +0100

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r ./requirements.txt
+
+python mysite/manage.py collectstatic --no-input
+python mysite/manage.py migrate
+
+echo "Done with Build!"
+
